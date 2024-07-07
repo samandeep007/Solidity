67f80eb6f8be81027cad92aeef0925f612fc0e5a
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-06 21:08:48 -0600

    chore: Update zombieFeeding contract to calculate new DNA based on target DNA

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,8 +1,11 @@
 pragma solidity>=0.5.0 < 0.6.0;
 import "./zombiefactory.sol";
 contract zombieFeeding is ZombieFactory {
-    function feedAndMultiply(uint _zombieId, uint _targetId) public {
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
       require(msg.sender == zombieToOwner[_zombieId]);
       Zombie storage myZombie = Zombies[_zombieId]
+      _targetDna = _targetDna % dnaModulus;
+      uint newDna = (myZombie.dna + _targetDna) / 2;
+      _createZombie("NoName", newDna);
     }
 }
