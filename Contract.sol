b4ec2da39cea260430059823978420eba252b745
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-06 18:43:16 -0600

    Update Solidity compiler version to v0.6.0

--- Contract.sol
+++ Contract.sol
@@ -1,32 +1,36 @@
-pragma solidity >= 0.5.0 < 0.7.0;
+pragma solidity >=0.5.0 <0.6.0;
 
-contract ZombieFactory{
+contract ZombieFactory {
 
     event NewZombie(uint zombieId, string name, uint dna);
+
     uint dnaDigits = 16;
-    uint dnaModulus =  10**dnaDigits;
+    uint dnaModulus = 10 ** dnaDigits;
 
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 
-struct Zombie{
-    string name;
-    uint dna;
-}
+    Zombie[] public zombies;
 
-Zombie[] public zombies;
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
 
-function _createZombie(string memory _name, uint _dna) private {
-    uint id = zombies.push(Zombie(_name, _dna)) - 1;
-    emit NewZombie(id, _name, _dna);
-}
+    function _createZombie(string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // start here
+        emit NewZombie(id, _name, _dna);
+    }
 
-function _generateRandomDna(string memory _str) private view returns(uint){
-    uint rand = uint(keccak256(abi.encodePacked(_str)));
-    return rand % dnaModulus;
-}
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
 
-function createRandomZombie(string memory _str) public {
-    uint randDna = _generateRandomDna(_str);
-    _createZombie(_str, randDna);
-}
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
 
 }
