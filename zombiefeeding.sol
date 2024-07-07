9db93c40d7f576838e2031afc68b16c0d37c61c1
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-06 21:02:40 -0600

    refactor: Add missing semicolon in zombieFeeding contract

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,5 +1,8 @@
 pragma solidity>=0.5.0 < 0.6.0;
 import "./zombiefactory.sol";
 contract zombieFeeding is ZombieFactory {
-
+    function feedAndMultiply(uint _zombieId, uint _targetId) public {
+      require(msg.sender == zombieToOwner[_zombieId]);
+      Zombie storage myZombie = Zombies[_zombieId]
+    }
 }
