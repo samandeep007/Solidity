0976323de5b7087043e2f6bda0979b38489ef8c9
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-06 22:36:57 -0600

    refactor: Update zombieFeeding contract to use lowercase variable name for zombies

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,6 +1,27 @@
 pragma solidity>=0.5.0 < 0.6.0;
 import "./zombiefactory.sol";
+
+//Creating kittyInterface
+contract kittyInterface{
+    function getKitty(uint256 _id) public view returns(
+      bool isGestating,
+      bool isReady,
+      uint256 cooldownIndex,
+      uint256 nextActionAt,
+      uint256 siringWithId,
+      uint256 birthTime,
+      uint256 matronId,
+      uint256 sireId,
+      uint256 generation,
+      uint256 genes;
+    );
+}
+
 contract zombieFeeding is ZombieFactory {
+
+    address ckaddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    kittyInterface kittyContract = kittyInterface(ckaddress);
+
 
 
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
@@ -9,6 +30,13 @@ contract zombieFeeding is ZombieFactory {
       _targetDna = _targetDna % dnaModulus;
       uint newDna = (myZombie.dna + _targetDna) / 2; // Average of zombie's dna and target dna
       _createZombie("NoName", newDna); //creating a zombie
+    }
+
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna);
+
     }
 
 
