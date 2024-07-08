2679053ee45ff665d5e51cfcc65eec356e53adc5
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-07 18:19:54 -0600

    refactor: Improve zombie feeding logic and DNA calculation

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -19,20 +19,32 @@ contract kittyInterface{
 
 contract zombieFeeding is ZombieFactory {
 
-    address ckaddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-    kittyInterface kittyContract = kittyInterface(ckaddress);
+    kittyInterface kittyContract;
+    function setKittyContractAddress(address _address) external onlyOwner{
+       kittyContract = kittyInterface(_address);
+    }
+
+    function _triggerCooldown(Zombie storage _zombie) internal {
+      _zombie.readyTime = uint32(now + cooldownTime);      
+    }
+
+    function _isReady(Zombie storage _zombie) internal view returns(bool){
+      return (_zombie.readyTime <= now);
+    }
 
 
 
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
       require(msg.sender == zombieToOwner[_zombieId]); // Authenticating
       Zombie storage myZombie = zombies[_zombieId]
+      require(_isReady(myZombie));
       _targetDna = _targetDna % dnaModulus;
       uint newDna = (myZombie.dna + _targetDna) / 2; // Average of zombie's dna and target dna
       if(keccak256(abi.encodePacked(_specied)) == keccak256(abi.encode("kitty"))){
         newDna = newDna - newDna % 100 + 99;
       }
       _createZombie("NoName", newDna); //creating a zombie
+      _triggerCooldown(myZombie);
     }
 
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
